dbbcec815427522fc48a5ddb2a44a3e4158b1fd2
Leonardo Ramos <leo.unicamp97@gmail.com>
2025-06-30 14:43:24 -0300

    First commit develop

---
+++ src/fs_geral.sql
@@ -0,0 +1,37 @@
+WITH Geral AS (
+    
+    SELECT A.idCustomer,
+       MAX(julianday('2024-07-04') - julianday(DATE(dtTransaction))) AS idade_base,
+       B.flEmail,
+       COUNT(A.idTransaction)/COUNT(DISTINCT(DATE(A.dtTransaction))) AS avg_transact_day,
+       SUM(CASE WHEN A.pointsTransaction > 0 THEN A.pointsTransaction ELSE 0 END) * 1.0
+       / COUNT(DISTINCT(DATE(A.dtTransaction))) AS avg_points_day
+FROM transactions A
+LEFT JOIN customers B
+ON A.idCustomer = B.idCustomer
+GROUP BY A.idCustomer 
+),
+
+Base_ativa AS (
+    SELECT idCustomer,
+    MIN(julianday('2024-07-04') - julianday(DATE(dtTransaction))) AS Recencia_21,
+    COUNT(DISTINCT(DATE(dtTransaction))) AS Frequencia_21,
+    SUM(CASE WHEN pointsTransaction > 0 THEN pointsTransaction ELSE 0 END) as Pontos_21
+    FROM transactions
+    WHERE dtTransaction < '2024-07-04'
+    AND dtTransaction >= DATE('2024-07-04','-21 DAY')
+    GROUP BY idCustomer
+)
+
+SELECT A.idCustomer, 
+    A.Recencia_21,
+    A.Frequencia_21,
+    A.Pontos_21,
+    B.idade_base,
+    B.avg_transact_day,
+    B.avg_points_day
+FROM Base_ativa A
+LEFT JOIN Geral B
+ON A.idCustomer = B.idCustomer
+
+
