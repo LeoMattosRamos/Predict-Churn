107c7ce79cc5b6ac48ddf9ef3e1b41ade41aba6a
Leonardo Ramos <leo.unicamp97@gmail.com>
2025-06-30 19:52:12 -0300

    fs_prod

--- src/fs_produtos.sql
+++ src/fs_produtos.sql
@@ -1,16 +1,16 @@
 SELECT 
     A.idCustomer,
-    SUM(CASE WHEN B.NameProduct = 'ChatMessage' THEN A.pointsTransaction ELSE 0 END)*1.0/SUM(A.pointsTransaction) AS ChatMessage,
-    SUM(CASE WHEN B.NameProduct = 'Resgatar Ponei' THEN A.pointsTransaction ELSE 0 END) AS Resgatar_Ponei,
-    SUM(CASE WHEN B.NameProduct = 'Lista de presença' THEN A.pointsTransaction ELSE 0 END) AS Lista_de_presenca,
-    SUM(CASE WHEN B.NameProduct = 'Presença Streak' THEN A.pointsTransaction ELSE 0 END) AS Presenca_Streak,
-    SUM(A.pointsTransaction) AS Total
+    ROUND(SUM(CASE WHEN B.NameProduct = 'ChatMessage' THEN A.pointsTransaction ELSE 0 END)*1.0/SUM(A.pointsTransaction),2) AS ChatMessage,
+    ROUND(SUM(CASE WHEN B.NameProduct = 'Resgatar Ponei' THEN A.pointsTransaction ELSE 0 END)*1.0/SUM(A.pointsTransaction),2)  AS Resgatar_Ponei,
+    ROUND(SUM(CASE WHEN B.NameProduct = 'Lista de presença' THEN A.pointsTransaction ELSE 0 END)*1.0/SUM(A.pointsTransaction),2)  AS Lista_de_presenca,
+    ROUND(SUM(CASE WHEN B.NameProduct = 'Presença Streak' THEN A.pointsTransaction ELSE 0 END)*1.0/SUM(A.pointsTransaction),2)  AS Presenca_Streak,
+    ROUND(SUM(CASE WHEN B.NameProduct = 'ChatMessage' THEN A.pointsTransaction ELSE 0 END)*1.0/COUNT(DISTINCT(DATE(A.dtTransaction))),2) AS Chatmessage_per_live
 FROM transactions A
 LEFT JOIN transactions_product B
 ON A.idTransaction = B.idTransaction
 WHERE B.NameProduct IN ('ChatMessage','Resgatar Ponei','Lista de presença','Presença Streak')
+      AND dtTransaction >= DATE('2024-07-04','-21 DAY')
+      AND dtTransaction < '2024-07-04'
+    
 GROUP BY 1
-
-
-
 
