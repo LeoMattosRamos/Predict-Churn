9bed052954860fa4ff6b5c847b8240976f5125c7
Leonardo Ramos <leo.unicamp97@gmail.com>
2025-07-07 12:39:05 -0300

    mlflow

--- src/train/ABT.sql
+++ src/train/ABT.sql
@@ -12,9 +12,9 @@ LEFT JOIN fs_geral B
 ON A.idCustomer = B.idCustomer
 AND A.Dt_ref = DATE(B.Dt_ref, "-21 DAY")
 
-WHERE A.Dt_ref < DATE("2024-06-01","-21 DAY") /* Garantir base maturada (data mais recente -21 dias)*/
+WHERE A.Dt_ref < DATE("2024-07-01","-21 DAY") /* Garantir base maturada (data mais recente -21 dias)*/
 AND strftime("%d", A.Dt_ref) = '01' /* Diminuir viés de usuários com muita interação - considearar as features 1 vez no mês para cada usuário - considera apenas dia 01 de cada mês)*/
-OR A.Dt_ref = DATE("2024-06-01") /* Usar como base out of time */
+OR A.Dt_ref = DATE("2024-07-01","21 DAY") /* Usar como base out of time */
 )
 
 SELECT 
@@ -58,9 +58,8 @@ AND A.Dt_ref = D.Dt_ref
 LEFT JOIN fs_transacoes E
 ON A.idCustomer = E.idCustomer
 AND A.Dt_ref = E.Dt_ref
-
-
-
+GROUP BY A.Dt_ref,
+      A.idCustomer
 
 
 
--- src/train/train.py
+++ src/train/train.py
@@ -7,6 +7,8 @@ from sklearn import metrics
 from sklearn import pipeline
 from sklearn import ensemble
 from sklearn import tree
+import scikitplot as skplt
+
 
 
 CREATE_ENGINE = sqlalchemy.create_engine("sqlite:///../../data/feature_store.db")
@@ -53,29 +55,34 @@ X_analise['diff_%'] = X_analise[0] / X_analise[1]
 
 X_analise.sort_values("diff_%", ascending=False)
 
+model = ensemble.RandomForestClassifier(random_state=42)
 
-params = {"min_samples_leaf":[10,20,30,40,50,100,200],
-          "max_depth": [2,4,5,6,8,10,15,20],
-          "criterion": ["gini", "entropy"],
-          "n_estimators": [10,20,50,100,200,300]
+params = {"model__min_samples_leaf":[10,20,30,40,50,100,200],
+          "model__max_depth": [2,4,5,6,8,10,15,20],
+          "model__criterion": ["gini", "entropy"],
+          "model__n_estimators": [10,20,50,100,200,300]
           }
 
-model = ensemble.RandomForestClassifier(random_state=42)
+
+model_pipeline = pipeline.Pipeline(
+     steps=[
+          ('model', model)
+     ]
+)
                                 
-grid = model_selection.GridSearchCV(model,
+grid = model_selection.GridSearchCV(model_pipeline,
                                     param_grid=params,
                                     cv=3,
                                     n_jobs= 8,
                                     scoring='roc_auc')
+
+
 
 grid.fit(X_train, y_train)
 
 best_model = grid.best_estimator_
 
 
-importances = pd.Series(data=best_model.feature_importances_, index= X)
-importances = importances.sort_values(ascending=False)
-importances
 
 
 y_pred_train = grid.predict(X_train)
@@ -84,17 +91,65 @@ y_predict_proba = grid.predict_proba(X_test)[:,1]
 y_pred_proba_oot = grid.predict_proba(df_oot[df_oot.columns[3:]])[:,1]
 y_pred_oot = grid.predict(df_oot[df_oot.columns[3:]])
 
+
+
 acc_treino = metrics.accuracy_score(y_train, y_pred_train)
 acc_test = metrics.accuracy_score(y_test, y_pred_test)
 roc = metrics.roc_auc_score(y_test, y_predict_proba)
 roc_oot = metrics.roc_auc_score(df_oot[y], y_pred_proba_oot)
 acc_oot = metrics.accuracy_score(df_oot[y],y_pred_oot )
-
+recall = metrics.recall_score(y_test, y_pred_test)
+recall_oot = metrics.recall_score(df_oot[y],y_pred_oot )
 
 print(f"acc_treino: {acc_treino}")
 print(f"acc_test: {acc_test}")
 print(f"roc: {roc}")
+print(f"recall: {recall}")
 print(f"roc_oot: {roc_oot}")
 print(f"acc_oot: {acc_oot}")
+print(f"recall_oot: {recall_oot}")
+
+
+confusion_matrix_teste = skplt.metrics.plot_confusion_matrix(y_test, y_pred_test)
+
+confusion_matrix_oot = skplt.metrics.plot_confusion_matrix(df_oot[y], y_pred_oot)
 
 #%%
+y_pred_proba_oot_ = grid.predict_proba(df_oot[df_oot.columns[3:]])
+y_predict_proba_ = grid.predict_proba(X_test)
+# ROC = skplt.metrics.plot_ks_statistic(df_oot[y],y_pred_proba_oot_ )
+# lift = skplt.metrics.plot_lift_curve(df_oot[y],y_pred_proba_oot_ )
+cumulative_gain = skplt.metrics.plot_cumulative_gain(df_oot[y],y_pred_proba_oot_)
+ks = skplt.metrics.plot_ks_statistic(y_test, y_predict_proba_)
+
+#%%
+
+import numpy as np
+
+y_predict_proba_treino = grid.predict_proba(X_train)[:,1]
+
+fpr, tpr, thresholds = metrics.roc_curve(y_test, y_predict_proba)
+youden = tpr - fpr
+optimal_idx = np.argmax(youden)
+optimal_thresh = thresholds[optimal_idx]
+
+y_pred_ajusted_test = (y_predict_proba >= optimal_thresh).astype(int) #testando outro threshold (youden = KS)
+y_pred_ajusted_oot = (y_pred_proba_oot>= optimal_thresh).astype(int)
+y_pred_ajusted_treino = (y_predict_proba_treino >= optimal_thresh).astype(int)
+
+Acc_ajusted_teste = metrics.accuracy_score(y_test, y_pred_ajusted_test)
+Acc_ajusted_treino = metrics.accuracy_score(y_train, y_pred_ajusted_treino)
+Acc_ajusted_oot = metrics.accuracy_score(df_oot[y], y_pred_ajusted_oot)
+
+print(f"acc_test: {acc_test}")
+print(f"Ajustado_teste{Acc_ajusted_teste}")
+
+print(f"acc_treino: {acc_treino}")
+print(f"Ajustado_treino{Acc_ajusted_treino}")
+
+print(f"acc_oot: {acc_oot}")
+print(f"Ajustado_oot{Acc_ajusted_oot}")
+
+
+## Nao obtivemos aumento de acc na base teste e OOT, apenas no treino. Portanto manterei o threshold padrão.
+
